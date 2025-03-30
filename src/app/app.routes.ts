08a09b9d9abe7e295d45a26d7b53f7f764d0250d
georggeo23 <georgechris2003@gmail.com>
2025-03-30 12:36:40 -0400

    app.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RemindersComponent } from './pages/reminders/reminders.component';
+import { TaskListComponent } from './pages/task-list/task-list.component';
+import { GoalListComponent } from './pages/goal-list/goal-list.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'goals',
+        component: GoalListComponent
+    },
+    {
+        path: 'tasks',
+        component: TaskListComponent
+    },
+    {
+        path: 'reminders',
+        component: RemindersComponent
+    }
+];
